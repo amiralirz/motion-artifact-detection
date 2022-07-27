d3089c8c7872a7666cf6fa770b3249fedfd7546b
Emertat <amiralirazi.2000@gmail.com>
2022-07-27 09:28:25 +0430

    Detecting R peaks using pan tompkins

--- motion_artifact_detector.py
+++ motion_artifact_detector.py
@@ -1,43 +1,44 @@
 from cmath import nan
 import pandas as pd
+import math
 import numpy as np
-#import matplotlib.pyplot as plt
+from Pan_Tompkins_QRS import *
+import matplotlib.pyplot as plt
 
-threshold = 0.115
+threshold = 15
 gap = 5
+fs = 200
 
-file_number = 23
+file_number = 19
 df = pd.read_csv('BW_ (' + str(file_number) + ').csv')
 
 ECG_data = list([float(x) for x in df['dataPoint']])
-print(ECG_data[100])
-R_peak_index = [98,243,384,456,639,763,885,1009,1134,1260,1381,1498,1610,1724,1848,1996] # this array is given by pan tompkins
+for i in range(len(ECG_data)):
+    ECG_data[i] = math.floor(ECG_data[i] * 400)
+
+QRS_detector = Pan_Tompkins_QRS()
+input_signal = pd.DataFrame(np.array([list(range(len(ECG_data))), ECG_data]).T, columns=['TimeStamp', 'ecg'])
+output_signal = QRS_detector.solve(input_signal)
+
+signal = np.array(ECG_data)
+hr = heart_rate(signal, fs)
+result = hr.find_r_peaks()
+result = np.array(result)
+result = result[result > 0]
+
+print(result)
+
+R_peak_index = result.copy()
 is_motion_artifact = [0] * (len(R_peak_index) - 1)
 
-# filling the gaps
-
-for i in range(len(ECG_data)):
-    if ECG_data[i] == nan:
-        j = 1
-        while ECG_data[i + j] == nan:
-            j = j + 1
-        step = (ECG_data[i + j] - ECG_data[i - 1]) / (j + 1)
-        for k in range(j):
-            ECG_data[i + k] = ECG_data[i -1] + step * (k + 1)
-        i = i + j
-
-m = np.mean(ECG_data)
-
 for i in range(len(R_peak_index) - 1):
-    section  = ECG_data[R_peak_index[i] + gap : R_peak_index[i+1] - gap].copy()
-    v = np.var(section)**0.5    
-    #print(v)    
+    section = ECG_data[R_peak_index[i] + gap: R_peak_index[i + 1] - gap].copy()
+    v = np.var(section) ** 0.5
     if v > threshold:
         is_motion_artifact[i] = 1
     else:
         is_motion_artifact[i] = 0
 
 print(is_motion_artifact)
-
 
 
