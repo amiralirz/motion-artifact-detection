7431a13cdd87653a061856d7d8ff13de24c9a3b5
amira <amiralirazi.2000@gmail.com>
2022-08-03 15:11:51 +0430

    getting input from the user and making wfdb annotation file

--- Data_cleaner.py
+++ Data_cleaner.py
@@ -1,12 +1,11 @@
 import math
-from cmath import nan
 
 
 def clean_up(input_signal):
     l = len(input_signal)  # length of the signal
     output_signal = input_signal.copy()
     for i in range(l):
-        if math.isnan(output_signal[i]):
+        if math.isnan(output_signal[i]): # checking if the input is nan
             j = 1
             while i + j < l:
                 if math.isnan(output_signal[i + j]):
@@ -15,13 +14,13 @@ def clean_up(input_signal):
                     break
             step = 0
             if i + j == l:
-                step = output_signal[i - 1] - output_signal[i - 2]
+                step = output_signal[i - 1] - output_signal[i - 2]  # fixing the end of the signal
             elif i == 0:
-                step = output_signal[i + j + 1] - output_signal[i + j]
+                step = output_signal[i + j + 1] - output_signal[i + j]  # fixing the start of the signal
             elif i == 0 and i + j == l:
-                return -1
+                return -1  # the whole signal is nan
             else:
-                step = (output_signal[i + j] - output_signal[i - 1]) / (j + 1)
+                step = (output_signal[i + j] - output_signal[i - 1]) / (j + 1)  # fixing the middle of the signal
             for k in range(j):
                 output_signal[i + k] = int(round(output_signal[i - 1] + step * (k + 1), 0))  # rounding to the closest integer
     return output_signal
--- motion_artifact_detector.py
+++ motion_artifact_detector.py
@@ -1,21 +1,51 @@
-from cmath import nan
+import numpy
 import pandas as pd
-import math
 import numpy as np
+import wfdb
 from Pan_Tompkins_QRS import *
 from Data_cleaner import *
 import matplotlib.pyplot as plt
+import os
+import bisect
 
-threshold = 40
-gap = 10
-fs = 200
+# file_number = 50
+# input_directory = r'C:\hmmmmmmmm\Work\Data\motion artifact data wfdb\wfdb40sec/'
+input_directory = input("input file directory : ") or "C:\hmmmmmmmm\Work\Data\motion artifact data wfdb\wfdb40sec"
+file_name = input("file name : ") or "082364"
+output_directory = input(
+    "output file directory : ") or "C:\hmmmmmmmm\Work\Data\motion artifact data wfdb\Sample_outputs"
+threshold = int(input("threshold : ") or '62')
 
-file_number = 17
-df = pd.read_csv('BW_ (' + str(file_number) + ').csv')  # reading the CSV file
+# this part is for reading csv files
 
-ECG_data = list([float(x) for x in df['dataPoint']])  #
+# file = directory + file_names[file_number]
+# df = pd.read_csv(file)  # reading the CSV file
+# ECG_data = df.values[:, 0]
+# ECG_data = list([float(x) for x in df['dataPoint']])
+
+# this part is for reading wfdb files
+
+record = wfdb.rdrecord(input_directory + '/' + file_name)
+ECG_data = record.p_signal[:, 0]
+annotation = wfdb.rdann(input_directory + '/' + file_name, 'rpeak')
+ann_symbol = annotation.symbol.copy()
+ann_sample = annotation.sample.copy().tolist()
+ann_subtype = annotation.subtype.copy().tolist()
+# print(annotation.sample)
+# print(annotation.symbol)
+# print(annotation.subtype)
+
+# wfdb.plot_wfdb(record, annotation, title=str(file_number))
+
+fs = record.fs
+gap = int(2 / 10 * fs)
 
 ECG_data = clean_up(ECG_data)  # cleaning the gaps in the data using extrapolation
+data_length = len(ECG_data)
+
+for i in range(len(ECG_data)):
+    if numpy.isnan(ECG_data[i]):
+        print(i)
 
 for i in range(len(ECG_data)):
     ECG_data[i] = math.floor(ECG_data[i] * 400)  # converting the data to io int type with minimum loss
@@ -36,22 +66,94 @@ is_motion_artifact = [0] * (len(R_peak_index) - 1)  # this is the primary output
 fig = [0] * len(ECG_data)  # this array is only used for presentation
 
 for i in range(len(R_peak_index) - 1):
-    section = ECG_data[R_peak_index[i] + gap: R_peak_index[i + 1] - gap].copy()  # dividing the data into sections
-    v = np.var(section) ** 0.5  # calculating the standard deviation of each section
-    fig[R_peak_index[i] + gap: R_peak_index[i + 1] - gap] = [v * 10] * len(section)  # this is only for presentation
-    if v > threshold:
-        is_motion_artifact[i] = 1
-    else:
-        is_motion_artifact[i] = 0
+    if R_peak_index[i] != R_peak_index[i + 1]:
+        section = ECG_data[R_peak_index[i] + gap: R_peak_index[i + 1] - gap].copy()  # dividing the data into sections
+        if len(section) == 0:
+            continue
+        v = np.var(section) ** 0.5  # calculating the standard deviation of each section
+        if v > threshold:
+            is_motion_artifact[i] = 1
+            fig[R_peak_index[i] + gap: R_peak_index[i+1] - gap] = [v * 10] * (R_peak_index[i+1] - gap - R_peak_index[i] - gap)
+        else:
+            is_motion_artifact[i] = 0
 
-print(is_motion_artifact)  # main output of the code
+noise_episode_start_index = 0
+noise_episode_end_index = 0
+noise_episode_started = False
+noise_episode_margin = 5
+noise_episode_margin_counter = noise_episode_margin
+noise_episode_starts = []
+noise_episode_ends = []
+noise_episodes = 0
+
+
+for i in range(len(is_motion_artifact)):  # finding and storing the index of noise episodes
+    if is_motion_artifact[i] == 1:
+        noise_episode_margin_counter = noise_episode_margin
+        if not noise_episode_started:
+            noise_episode_started = True
+            noise_episode_start_index = max(0, R_peak_index[max(0, i - noise_episode_margin_counter)] - gap)
+            if i < noise_episode_margin:
+                noise_episode_start_index = 0
+    else:
+        if noise_episode_started:
+            noise_episode_margin_counter -= 1
+            if noise_episode_margin_counter == 0:
+                noise_episode_margin_counter = noise_episode_margin
+                noise_episode_started = False
+                last_index = len(R_peak_index) - 1
+                noise_episode_end_index = min(R_peak_index[min(i + 1, last_index)] + gap, data_length - 1)
+                noise_episode_ends.append(noise_episode_end_index)
+                noise_episode_starts.append(noise_episode_start_index)
+                noise_episodes += 1
+
+if noise_episodes > 1:
+    i = 0
+    while i < noise_episodes:  # merging noise episodes that overlap
+        if i > 0:
+            if noise_episode_starts[i] < noise_episode_ends[i - 1]:
+                noise_episode_starts.remove(noise_episode_starts[i])
+                noise_episode_ends.remove(noise_episode_ends[i - 1])
+                noise_episodes -= 1
+        i += 1
+
+for i in range(noise_episodes):  # adding the episodes to tha annotation file
+
+    bisect.insort(ann_sample, noise_episode_starts[i])  # adding the start index
+    insertion_index = ann_sample.index(noise_episode_starts[i])
+    ann_symbol.insert(insertion_index, '~')
+    ann_subtype.insert(insertion_index, 1)
+
+    bisect.insort(ann_sample, noise_episode_ends[i])  # adding the end index
+    insertion_index = ann_sample.index(noise_episode_ends[i])
+    ann_symbol.insert(insertion_index, '~')
+    ann_subtype.insert(insertion_index, 0)
+
+for i in range(len(ann_sample)):
+    if ann_symbol[i] == '~':
+        print(f"{ann_sample[i]} : {ann_symbol[i]} : {ann_subtype[i]}")
+
+wfdb.wrann(file_name, 'atr', np.array(ann_sample), ann_symbol, np.array(ann_subtype), np.array([0] * len(ann_sample)),
+           np.array([0] * len(ann_sample)),
+           [''] * len(ann_sample), fs=record.fs,
+           write_dir="C:\hmmmmmmmm\Work\Data\motion artifact data wfdb\Sample_outputs")
+
+if is_motion_artifact.count(1) > 0:
+    print(f'the sample has Motion Artifact : {is_motion_artifact.count(1)}')
+
+ai = []
+for i in range(len(ann_symbol)):
+    if ann_symbol[i] == '~':
+        ai.append(ann_sample[i])
+
+print(ai)
+bi = [4000] * len(ai)
 
 plt.figure(figsize=(18, 8), dpi=100)
-plt.plot(ECG_data)
-plt.plot(fig)
-plt.scatter(result, signal[result], color='red', s=50,  marker='*')
-plt.title('file no.' + str(file_number))
+plt.plot(ECG_data)  # ECG data
+plt.plot(fig)  # motion artifact data
+plt.scatter(result, signal[result], color='red', s=50, marker='*')  # peaks
+plt.stem(ai, bi, 'g')
+plt.title('file : ' + file_name)
 plt.xlabel('samples')
 plt.show()
-
-
